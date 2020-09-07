cb5745d696ad91aa9a903a800b3218cf603bcd99
Manh Pham <manhp@newit.co.jp>
2020-09-07 22:57:52 +0700

    initialize relewase

---
+++ swiper-vue.esm.js
@@ -0,0 +1,15 @@
+/**
+ * Swiper Vue 6.4.0
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * http://swiperjs.com
+ *
+ * Copyright 2014-2020 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: September 7, 2020
+ */
+
+import { Swiper } from './esm/vue/swiper';
+import { SwiperSlide } from './esm/vue/swiper-slide';
+export { Swiper, SwiperSlide };
